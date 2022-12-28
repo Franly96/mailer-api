4255f99746795a971af61b4b4ed9db213f7a6cde
Franly Pena <22371923+Franly96@users.noreply.github.com>
2022-12-28 11:07:58 -0400

    update mail controller test

--- src/mail/mail.controller.spec.ts
+++ src/mail/mail.controller.spec.ts
@@ -1,6 +1,8 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
+import { Mail, MailSchema } from './schemas/mail.schema';
 
 describe('MailController', () => {
   let controller: MailController;
@@ -8,7 +10,10 @@ describe('MailController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MailController],
-      providers: [MailService],
+      providers: [
+        MailService,
+        { provide: getModelToken(Mail.name), useValue: MailSchema },
+      ],
     }).compile();
 
     controller = module.get<MailController>(MailController);
